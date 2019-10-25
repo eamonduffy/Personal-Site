71836f4fb383ebc5968111d7fbea2a3f478777ae
Eamon Duffy <eamon.duffy0@gmail.com>
2019-10-25 16:36:04 -0400

    adding fontawesome2

---
+++ src/Pages/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return <Container>hello eamon</Container>;
+}
+
+export default App;
