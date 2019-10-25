a55370e11fca6ccdc8e3b0c9cd3f3c5773cdf8cd
Eamon Duffy <eamon.duffy0@gmail.com>
2019-10-25 16:35:29 -0400

    adding fontawesome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./Pages/App";
 import * as serviceWorker from "./serviceWorker";
+import "./fontawesome";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
