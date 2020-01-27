e23c5c38fec10f949715577a5ffe39230defca89
Eamon Duffy <eamon.duffy0@gmail.com>
2020-01-27 10:50:19 -0500

    back at it2

---
+++ src/Components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>Side</Col>
+        <Col>Main</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Components/SidenavBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { Container, Button } from 'react-bootstrap';
+
+export default function SidenavBtn() {
+  return (
+    <Container>
+      <Button variant="primary">Primary</Button>
+    </Container>
+  );
+}
