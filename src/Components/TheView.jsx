3f5b9a533016e69fc5b314f6e56004573c8c2d07
Eamon Duffy <eamon.duffy0@gmail.com>
2020-01-31 13:46:27 -0500

    more to come

--- src/Components/TheView.jsx
+++ src/Components/TheView.jsx
@@ -5,13 +5,14 @@ import styled from 'styled-components';
 // COMPONENTS
 
 const View = styled.div`
-  // background-color: rgba(124, 124, 124, 0.5);
-  height: 85vh;
+  background-color: rgba(124, 124, 124, 0.5);
+  height: 84vh;
   width: 100%;
+  margin-left: 100px;
+  margin-right: 100px;
+  border-radius: 15px;
 `;
 
 export default function TheView() {
-  return (
-    <View></View>
-  );
+  return <View></View>;
 }
