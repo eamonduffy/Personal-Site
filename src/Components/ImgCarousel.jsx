c4065b6639fbb65232040e32b1aee5676d001f32
Eamon Duffy <eamon.duffy0@gmail.com>
2020-02-04 15:09:03 -0500

    carousel completion

--- src/Components/ImgCarousel.jsx
+++ src/Components/ImgCarousel.jsx
@@ -1,42 +1,48 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import me from '../img/meProfile.PNG'
+
+// Pictures
+import me from '../img/meProfile.PNG';
+import pic2 from '../img/2.jpg';
+import pic3 from '../img/3.jpg';
+import pic4 from '../img/4.jpg';
+
+const imgFit = {
+  maxWidth: '95%',
+  maxHeight: '80vh',
+  margin: '15px',
+  // paddingLeft: '10px',
+  borderRadius: '5px',
+  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+};
 
 export default function ImgCarousel() {
-
   return (
     <Carousel>
       <Carousel.Item>
         <img
-          className="d-block"
-          src={me}
+          className="d-block w-100"
+          src={pic2}
           alt="First slide"
+          style={imgFit}
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Third slide"
+          src={pic3}
+          alt="Second slide"
+          style={imgFit}
         />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          src={pic4}
           alt="Third slide"
+          style={imgFit}
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-  )
+  );
 }
