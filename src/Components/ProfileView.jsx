04ff2a68fe87a7c49aa0cd79e6231a18aefe5572
Eamon Duffy <eamon.duffy0@gmail.com>
2020-02-06 08:46:45 -0500

    media changes

--- src/Components/ProfileView.jsx
+++ src/Components/ProfileView.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Row, Col } from 'react-bootstrap';
-import ImgCarousel from './ImgCarousel'
-import me from '../img/meProfile.PNG'
+import ImgCarousel from './ImgCarousel';
+import me from '../img/meProfile.PNG';
 
 const ProView = styled.div`
   z-index: 100;
-`
-const ImgContainer = styled.div`
-  display: absolute;
-  margin-left: auto;
-  margin-right:auto;
-  text-align: center;
-  position: relative;
-  top: 80%;
-`
+`;
 
+const Name = styled.h1`
+  margin: 15px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  color: white;
+  text-shadow: 2px 3px 3px #000000;
+`;
 
 export default function ProfileView(props) {
-
   return (
     <ProView>
       <Row>
         <Col>
           <ImgCarousel></ImgCarousel>
-          {/* <ImgContainer><img src={me} /></ImgContainer> */}
         </Col>
-        <Col></Col>
+        <Col>
+          <Row>
+            <Name>John Eamon Duffy</Name>
+          </Row>
+        </Col>
       </Row>
     </ProView>
   );
