7977ada51ef6d3f82c930808d607eca2b9be61af
Eamon Duffy <eamon.duffy0@gmail.com>
2020-03-02 09:19:40 -0500

    screen res changes

--- src/Components/ProjectsView.jsx
+++ src/Components/ProjectsView.jsx
@@ -15,6 +15,10 @@ const Name = styled.h1`
   margin-right: auto;
   color: white;
   text-shadow: 2px 3px 3px #000000;
+
+  @media (max-width: 1600px) {
+    font-size: 35px;
+  }
 `;
 
 const ProjectContainer = styled.div`
@@ -27,12 +31,21 @@ const ProjectContainer = styled.div`
   margin-right: auto;
   border-radius: 3px;
   text-align: center;
+
+  @media (max-width: 1600px) {
+    height: 165px;
+    margin-top: 15px;
+  }
 `;
 
 const ProjectTitle = styled.h4`
   margin-left: 40px;
   color: white;
   text-shadow: 2px 3px 3px #000000;
+
+  @media (max-width: 1600px) {
+    font-size: 22px;
+  }
 `;
 
 const ProjectDesc = styled.p`
@@ -41,6 +54,12 @@ const ProjectDesc = styled.p`
   color: white;
   text-shadow: 2px 3px 3px #000000;
   text-align: left;
+
+  @media (max-width: 1600px) {
+    font-size: 14px;
+    margin-right: 20px;
+    margin-bottom: 10px;
+  }
 `;
 
 const GitLink = styled.a`
@@ -52,8 +71,11 @@ const GitLink = styled.a`
   &:hover {
     color: #de6c64;
   }
-`
 
+  @media (max-width: 1600px) {
+    font-size: 14px;
+  }
+`;
 
 export default function ProjectsView(props) {
   return (
@@ -68,19 +90,31 @@ export default function ProjectsView(props) {
               <ProjectTitle>QU Weather</ProjectTitle>
             </Row>
             <Row>
-              <ProjectDesc>A web application that pulls weather data from New Haven and Quinnipiac sensors to store and display data for the public to view</ProjectDesc>
+              <ProjectDesc>
+                A web application that pulls weather data from New Haven and
+                Quinnipiac sensors to store and display data for the public to
+                view.
+              </ProjectDesc>
             </Row>
             <Row>
-              <AiFillTool color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
-              <ProjectDesc style={{ marginLeft: '10px' }}>React.js • Node.js</ProjectDesc>
+              <AiFillTool
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
+              <ProjectDesc style={{ marginLeft: '10px' }}>
+                React.js • Node.js
+              </ProjectDesc>
             </Row>
             <Row>
-              <FaRegBuilding color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
-              <ProjectDesc style={{ marginLeft: '10px' }}>Quinnipiac University</ProjectDesc>
+              <FaRegBuilding
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
+              <ProjectDesc style={{ marginLeft: '10px' }}>
+                Quinnipiac University
+              </ProjectDesc>
             </Row>
           </ProjectContainer>
         </Col>
@@ -91,19 +125,31 @@ export default function ProjectsView(props) {
                 <ProjectTitle>QU Rock Climbing Website</ProjectTitle>
               </Row>
               <Row>
-                <ProjectDesc>A web application made in collaboration to showcase the club and allow admins to post club information, events/trips and manage members</ProjectDesc>
+                <ProjectDesc>
+                  A web application made in collaboration to showcase the club
+                  and allow admins to post club information, events/trips and
+                  manage members.
+                </ProjectDesc>
               </Row>
               <Row>
-                <AiFillTool color="#DCDCDC"
-                  size="25"
-                  style={{ marginLeft: '40px' }} />
-                <ProjectDesc style={{ marginLeft: '10px' }}>Angular • Node.js</ProjectDesc>
+                <AiFillTool
+                  color="#DCDCDC"
+                  size="22"
+                  style={{ marginLeft: '40px' }}
+                />
+                <ProjectDesc style={{ marginLeft: '10px' }}>
+                  Angular • Node.js
+                </ProjectDesc>
               </Row>
               <Row>
-                <FaGitAlt color="#DCDCDC"
-                  size="25"
-                  style={{ marginLeft: '40px' }} />
-                <GitLink href="https://github.com/Hanlon199/QU-Rock-Climbing">Hanlon199/QU-Rock-Climbing</GitLink>
+                <FaGitAlt
+                  color="#DCDCDC"
+                  size="22"
+                  style={{ marginLeft: '40px' }}
+                />
+                <GitLink href="https://github.com/Hanlon199/QU-Rock-Climbing">
+                  Hanlon199/QU-Rock-Climbing
+                </GitLink>
               </Row>
             </ProjectContainer>
           </Col>
@@ -116,19 +162,30 @@ export default function ProjectsView(props) {
               <ProjectTitle>DevOps Tools Dashboard</ProjectTitle>
             </Row>
             <Row>
-              <ProjectDesc>A web application which showcases the structure of and flow of DevOps and gives access to tools used throughout the company.</ProjectDesc>
+              <ProjectDesc>
+                A web application which showcases the structure of and flow of
+                DevOps and gives access to tools used throughout the company.
+              </ProjectDesc>
             </Row>
             <Row>
-              <AiFillTool color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
-              <ProjectDesc style={{ marginLeft: '10px' }}>Vue.js • Node.js</ProjectDesc>
+              <AiFillTool
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
+              <ProjectDesc style={{ marginLeft: '10px' }}>
+                Vue.js • Node.js
+              </ProjectDesc>
             </Row>
             <Row>
-              <FaRegBuilding color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
-              <ProjectDesc style={{ marginLeft: '10px' }}>The Hartford, Inc.</ProjectDesc>
+              <FaRegBuilding
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
+              <ProjectDesc style={{ marginLeft: '10px' }}>
+                The Hartford, Inc.
+              </ProjectDesc>
             </Row>
           </ProjectContainer>
         </Col>
@@ -139,19 +196,31 @@ export default function ProjectsView(props) {
               <ProjectTitle>Five Stars</ProjectTitle>
             </Row>
             <Row>
-              <ProjectDesc>Mobile app made in collaboration to allow users to create accounts to rate movies, anime and video games which are posted for friends to view.</ProjectDesc>
+              <ProjectDesc>
+                Mobile app made in collaboration to allow users to create
+                accounts to rate movies, anime and video games which are posted
+                for friends to view.
+              </ProjectDesc>
             </Row>
             <Row>
-              <AiFillTool color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
-              <ProjectDesc style={{ marginLeft: '10px' }}>Android Application</ProjectDesc>
+              <AiFillTool
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
+              <ProjectDesc style={{ marginLeft: '10px' }}>
+                Android Application
+              </ProjectDesc>
             </Row>
             <Row>
-              <FaGitAlt color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
-              <GitLink href="https://github.com/Gorman98/Five-Stars">Gorman98/Five-Stars</GitLink>
+              <FaGitAlt
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
+              <GitLink href="https://github.com/Gorman98/Five-Stars">
+                Gorman98/Five-Stars
+              </GitLink>
             </Row>
           </ProjectContainer>
         </Col>
@@ -163,19 +232,28 @@ export default function ProjectsView(props) {
               <ProjectTitle>Company Bulletin Board</ProjectTitle>
             </Row>
             <Row>
-              <ProjectDesc>A web application that displays upcoming events, general company info and useful tips for employees and visitors.</ProjectDesc>
+              <ProjectDesc>
+                A web application that displays upcoming events, general company
+                info and useful tips for employees and visitors.
+              </ProjectDesc>
             </Row>
             <Row>
-              <AiFillTool color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
+              <AiFillTool
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
               <ProjectDesc style={{ marginLeft: '10px' }}>Vue.js</ProjectDesc>
             </Row>
             <Row>
-              <FaRegBuilding color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
-              <ProjectDesc style={{ marginLeft: '10px' }}>Precision Combustion, Inc.</ProjectDesc>
+              <FaRegBuilding
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
+              <ProjectDesc style={{ marginLeft: '10px' }}>
+                Precision Combustion, Inc.
+              </ProjectDesc>
             </Row>
           </ProjectContainer>
         </Col>
@@ -185,23 +263,32 @@ export default function ProjectsView(props) {
               <ProjectTitle>Wave Game</ProjectTitle>
             </Row>
             <Row>
-              <ProjectDesc>Improved an application as 5 man team to complete customer requirements given to us over the duration of a semester.</ProjectDesc>
+              <ProjectDesc>
+                Improved an application as 5 man team to complete customer
+                requirements given to us over the duration of a semester.
+              </ProjectDesc>
             </Row>
             <Row>
-              <AiFillTool color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
+              <AiFillTool
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
               <ProjectDesc style={{ marginLeft: '10px' }}>Java</ProjectDesc>
             </Row>
             <Row>
-              <FaGitAlt color="#DCDCDC"
-                size="25"
-                style={{ marginLeft: '40px' }} />
-              <GitLink href="https://github.com/WillEccles/wavegame">WillEccles/wavegame</GitLink>
+              <FaGitAlt
+                color="#DCDCDC"
+                size="22"
+                style={{ marginLeft: '40px' }}
+              />
+              <GitLink href="https://github.com/WillEccles/wavegame">
+                WillEccles/wavegame
+              </GitLink>
             </Row>
           </ProjectContainer>
         </Col>
       </Row>
-    </ProView >
+    </ProView>
   );
 }
